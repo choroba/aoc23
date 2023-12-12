c33bbe0657bf237da1dbe1364003214c0230c1c2
E. Choroba <choroba@matfyz.cz>
2023-12-12 11:11:59 +0100

    Optimize Day 12 Part 2

--- 12b.pl
+++ 12b.pl
@@ -6,19 +6,18 @@ use experimental qw( signatures );
 
 use ARGV::OrDATA;
 
-use Memoize qw{ memoize };
+my %cache;
+sub count($left, @g) {
+    return $cache{$left}{"@g"} if exists $cache{$left}
+                               && exists $cache{$left}{"@g"};
 
-sub count($left, $right) {
-    my @g = split /,/, $right;
     my $current_group_length = 0;
-    my $pos = 0;
-    while ($pos < length $left) {
+    my $pos = -1;
+    while (++$pos < length $left) {
         my $char = substr $left, $pos, 1;
 
         if ('#' eq $char) {
-            ++$current_group_length;
-            my @h = ("", @g);
-            return 0 if ! @g || $current_group_length > $g[0];
+             return 0 if ! @g || ++$current_group_length > $g[0];
 
         } elsif ('?' eq $char) {
             last
@@ -34,31 +33,27 @@ sub count($left, $right) {
                 return 0
             }
         }
-    } continue {
-        ++$pos
     }
 
-    shift @g if @g
-             && $pos == length $left
-             && $left =~ /#$/
-             && $current_group_length == $g[0];
-
-    return 1 if ! @g && $pos == length $left;;
+    if ($pos == length $left && (! @g || $current_group_length == $g[0])) {
+        shift @g;
+        return 1 unless @g;
+    }
 
     if ($pos < length $left) {
-        my $one = $left =~ s/\?/./r;
-        my $two = $left =~ s/\?/#/r;
+        my $one = $left;
+        substr $one, $pos, 1, '.';
+        my $two = $left;
+        substr $two, $pos, 1, '#';
         $one = substr $one, $pos if 0 == $current_group_length
                                  && '.' eq substr $one, $pos + 1, 1;
         $two = substr $two, $pos if 0 == $current_group_length
                                  && '.' eq substr $two, $pos + 1, 1;
-        $one =~ s/\.+/./g;
-        $one =~ s/^\.//;
+        $one =~ s/\.\././;
         my $g = join ',', @g;
-        return count($one, $g) + count($two, $g)
+        return $cache{$left}{"@g"} = count($one, @g) + count($two, @g)
     }
 }
-memoize('count');
 
 my $count = 0;
 while (<>) {
@@ -68,7 +63,8 @@ while (<>) {
     $right = join ',', ($right) x 5;
     $left =~ s/\.+/./g;
     $left =~ s/^\.//;
-    $count += count($left, $right);
+    $left =~ s/\.$//;
+    $count += count($left, split /,/, $right);
 }
 
 say $count;
