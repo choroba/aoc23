80dc7e3e0df69948f8f3bb3c5f7f91cf7c1b4e93
E. Choroba <choroba@matfyz.cz>
2023-12-17 22:36:25 +0100

    Add kind of a visualisation of day 17

---
+++ 17bv.pl
@@ -0,0 +1,114 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use feature qw{ say };
+
+use ARGV::OrDATA;
+use List::Util qw{ min max };
+use Term::ANSIColor qw{ colored };
+
+use constant {  # Use 1, 3 to solve Part 1.
+    MIN_STRAIGHT => 4,
+    MAX_STRAIGHT => 10,
+};
+
+sub clear { print "\e[2J"; cgoto(0, 0) }
+sub cgoto { my @o = map 1 + $_, @_; print "\e[$o[0];$o[1]f" }
+
+*STDOUT->autoflush(1);
+END { print "\e[?25h" }  # Reveal cursor.
+clear();
+print "\e[?25l";  # hide cursor
+
+my @DIRECTIONS = ([0, 1], [1, 0], [-1, 0], [0, -1]);
+
+my @map;
+while (<>) {
+    chomp;
+    push @map, [split //];
+}
+
+# heat[y][x][dir][straight]
+my @heat;
+$heat[0][0][2][ MAX_STRAIGHT - 1 ] = 0;
+$heat[0][0][3][ MAX_STRAIGHT - 1 ] = 0;
+
+while (1) {
+    my $change;
+    for my $y (0 .. $#map) {
+        for my $x (0 .. $#{ $map[0] }) {
+            next unless $heat[$y][$x];
+
+            for my $d_out (0 .. $#DIRECTIONS) {
+                my $direction_out = $DIRECTIONS[$d_out];
+                my $ny = $y + $direction_out->[0];
+                my $nx = $x + $direction_out->[1];
+                next if $ny < 0 || $ny > $#map
+                     || $nx < 0 || $nx > $#{ $map[0] };
+
+                for my $d_in (0 .. $#DIRECTIONS) {
+                    my $direction_in = $DIRECTIONS[$d_in];
+                    next if $direction_in->[0] == -$direction_out->[0]
+                         && $direction_in->[1] == -$direction_out->[1];
+
+                    if ($d_in == $d_out) {
+                        for my $s (1 .. MAX_STRAIGHT - 1) {
+                            next unless $heat[$y][$x][$d_out][$s - 1];
+
+                            my $h = $heat[$y][$x][$d_in][$s - 1]
+                                  + $map[$ny][$nx];
+                            ++$change, $heat[$ny][$nx][$d_out][$s] = $h
+                                if ! defined $heat[$ny][$nx][$d_out][$s]
+                                || $h < $heat[$ny][$nx][$d_out][$s];
+                        }
+                    } else {
+                        my $best = min(grep defined,
+                                       @{ $heat[$y][$x][$d_in] // [] }[
+                                           MIN_STRAIGHT - 1 .. MAX_STRAIGHT - 1
+                                       ]);
+                        next unless defined $best;
+
+                        my $h = $map[$ny][$nx] + $best;
+                        ++$change, $heat[$ny][$nx][$d_out][0] = $h
+                            if ! defined $heat[$ny][$nx][$d_out][0]
+                            || $h < $heat[$ny][$nx][$d_out][0];
+                    }
+                }
+            }
+        }
+    }
+    last unless $change;
+
+    cgoto(0, 0);
+    for my $y (0 .. $#map) {
+        for my $x (0 .. $#{ $map[0] }) {
+            my $v = min(grep defined, map @{ $_ // [] }, @{ $heat[$y][$x] });
+            $v //= 0;
+            my $c = chr($v / 51 + ord 'A');
+            my $g = 23 - $v / 1300 * 23;
+            my $bb = int($map[$y][$x] / 2) . ($map[$y][$x] % 2) . '0';
+            print colored($c, sprintf 'grey%d on_rgb%s', $g, $bb);
+        }
+        print "\n";
+    }
+    say $change, ' ' x 20;
+}
+
+say min(grep defined,
+        map @{ $_ // [] }[MIN_STRAIGHT - 1 .. MAX_STRAIGHT - 1],
+        @{ $heat[$#map][ $#{ $map[0] } ] });
+
+__DATA__
+2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533
